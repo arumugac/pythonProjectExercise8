72f4919170aa4bca66c5a9482b93a3d4656cc9a1
arumugac <chithra_thasan@yahoo.co.uk>
2022-10-26 00:32:24 +0100

    Update 2 files

--- types_practice.py
+++ types_practice.py
@@ -1,28 +1,23 @@
-score = int(input("Enter your Score:"))
-if score > 100 or score < 0:
-    print("That is invalid")
-else:
-    if score > 90:
-        print("Your grade is an A*")
-    elif score > 80:
-        print("Your grade is an A")
-    elif score > 70:
-        print("Your grade is an B")
-    elif score > 60:
-        print("Your grade is an C")
-    elif score > 50:
-        print("Your grade is an D")
-    elif score > 40:
-        print("Your grade is an E")
+import re
+
+password = input("Insert your password:")
+valid = "no"
+while valid == "no":
+    if len(password) < 6:
+        valid = "no"
+    elif not re.search("[a-z]", password):
+        valid = "no"
+    elif not re.search("[0-9]", password):
+        valid = "no"
+    elif not re.search("[A-Z]", password):
+        valid = "no"
+    elif not re.search("[!$#@£%&*_+={}?<>]", password):
+        valid = "no"
+
     else:
-        print("You Failed")
+        valid = "yes"
+        print("Valid Password")
 
-
-
-
-
-
-
-
-
-
+    if valid == "no":
+        print("Not a Valid Password")
+    break
