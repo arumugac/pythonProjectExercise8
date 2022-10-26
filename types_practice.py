53bf129451b65900a1da291654270ad122596908
arumugac <chithra_thasan@yahoo.co.uk>
2022-10-26 17:10:15 +0100

    Update 2 new changes

--- types_practice.py
+++ types_practice.py
@@ -1,17 +1,31 @@
-num = int(input("Please Enter a Number:"))
+import random
 
-if num > 1:
-    for number in range(num):
-        for i in range(2, number):
-            if number % i == 0:
-                break
+while True:
+    user_action = input("Enter a choice (rock, paper, scissors): ")
+    possible_actions = ["rock", "paper", "scissors"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if computer_action == "scissors":
+            print("Rock smashes scissors! You win!")
         else:
-            print(number)
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if computer_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if computer_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
 
-
-
-
-
-
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
 
 
